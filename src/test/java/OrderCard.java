83d70fc8065bf12d12de430e6901bdb26f57286b
OlgaNiki <oliadrok@gmail.com>
2023-12-26 21:06:28 +0300

    First commit. Initialization CI

--- src/test/java/OrderCard.java
+++ src/test/java/OrderCard.java
@@ -1,2 +1,8 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class OrderCard {
+
 }
