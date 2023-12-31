bc7ce6ecc88413f6c235950c65e7c1d944f74807
OlgaNiki <oliadrok@gmail.com>
2023-12-31 17:52:58 +0300

    First commit.

---
+++ src/test/java/OrderCardTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+
+public class OrderCardTest {
+
+    @Test
+    public void shouldBe (){
+        open(http://localhost:9999)
+    }
+
+}
