44d960a4598db8ba48f1dd2825f352269b873588
OlgaNiki <oliadrok@gmail.com>
2023-12-31 18:47:12 +0300

    First commit.

--- src/test/java/OrderCardTest.java
+++ src/test/java/OrderCardTest.java
@@ -5,10 +5,10 @@ import static com.codeborne.selenide.Condition.*;
 
 
 public class OrderCardTest {
-
     @Test
     public void shouldBe (){
-        open(http://localhost:9999)
+       open("http://localhost:9999");
+       $("[data-test-id=city] input").setValue("Казань");
     }
 
 }
